cfe4850a18f62d57fdb4188677e71b5d1e0045df
paderesbryan08@gmail.com <paderesbryan08@gmail.com>
2025-05-10 12:55:01 +0800

    Add project files.

---
+++ MobileAppIoTController/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MobileAppIoTController;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
